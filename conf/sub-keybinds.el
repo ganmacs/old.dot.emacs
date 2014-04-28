b1409ece4fb35821ec3634bbbb4058e44763cf52
ganmacs <ganmacs@gmail.com>
2014-04-29 00:31:13 +0900

    for scheme

--- conf/sub-keybinds.el
+++ conf/sub-keybinds.el
@@ -9,6 +9,31 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; @OverwreiteKeybind ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; (ido-mode nil)
 
+(load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+;; -----------sheme--------------------
+
+(setq process-coding-system-alist
+      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+(setq scheme-program-name "/usr/local/bin/gosh -i")
+
+(defun scheme-other-window ()
+  "Run Gauche on other window"
+  (interactive)
+  (split-window-horizontally (/ (frame-width) 2))
+  (let ((buf-name (buffer-name (current-buffer))))
+    (scheme-mode)
+    (switch-to-buffer-other-window
+     (get-buffer-create "*scheme*"))
+    (run-scheme scheme-program-name)
+    (switch-to-buffer-other-window
+     (get-buffer-create buf-name))))
+
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
+(define-key global-map "\C-cG" 'scheme-other-window)
+
+
 ;; ------------C Hook------------------
 (add-hook 'c-mode-common-hook
           '(lambda ()
